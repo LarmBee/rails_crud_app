2e037aaac7eec690bec68d0e05d2f0e53f99f8ff
Brandon <brandonkanute@gmail.com>
2022-08-08 01:41:16 +0300

    Final Commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/login'
+  # get 'sessions/welcome'
+  # get 'users/new'
+  # get 'users/create'
+  resources :beers
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
